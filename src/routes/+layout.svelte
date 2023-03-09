6d6ae5e07223132d9bc6610703c27bc0d657b8fd
Jake Cheek <jake@f-m.fm>
2023-03-09 17:36:35 -0500

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,5 +10,12 @@
 </script>
 
 <QueryClientProvider client={data.trpc.queryClient}>
-  <slot />
+  <div class="absolute top-0 left-0 h-screen w-screen bg-gray-800 text-white">
+    <nav class="m-1 rounded bg-black p-1 px-2">
+      <a href="/">Home</a>
+      <a href="/tracks">Tracks</a>
+    </nav>
+
+    <slot />
+  </div>
 </QueryClientProvider>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      primary: colors.amber,
+      secondary: colors.sky,
+      gray: colors.stone,
+
+      success: colors.green,
+      info: colors.blue,
+      warning: colors.yellow,
+      error: colors.red,
+
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+    },
     extend: {},
   },
   plugins: [],
