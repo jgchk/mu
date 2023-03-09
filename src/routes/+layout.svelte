3887c75a71d04a8cb8e77dbc6618c11ebf8d3493
Jake Cheek <jake@f-m.fm>
2023-03-09 17:43:09 -0500

    layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,12 +10,16 @@
 </script>
 
 <QueryClientProvider client={data.trpc.queryClient}>
-  <div class="absolute top-0 left-0 h-screen w-screen bg-gray-800 text-white">
-    <nav class="m-1 rounded bg-black p-1 px-2">
+  <div
+    class="absolute top-0 left-0 flex h-screen w-screen flex-col gap-1 bg-gray-800 p-1 text-white"
+  >
+    <nav class="rounded bg-black p-1 px-2">
       <a href="/">Home</a>
       <a href="/tracks">Tracks</a>
     </nav>
 
-    <slot />
+    <main class="flex-1 overflow-auto">
+      <slot />
+    </main>
   </div>
 </QueryClientProvider>
