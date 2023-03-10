db4c41f7ddd8ba5c686db10046e656e7fb699f1d
Jake Cheek <jake@f-m.fm>
2023-03-10 14:27:59 -0500

    ssr search results

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,6 +1,10 @@
 import type { PageLoad } from './$types'
 
-export const load: PageLoad = ({ url }) => {
+export const load: PageLoad = async ({ url, parent }) => {
   const query = url.searchParams.get('q')
+
+  const { trpc } = await parent()
+  await trpc.search.prefetchQuery({ query })
+
   return { query }
 }
