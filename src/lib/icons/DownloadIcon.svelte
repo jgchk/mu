3f7d29ec79c8014bb450b835e666741ab672fc07
Jake Cheek <jake@f-m.fm>
2023-03-09 23:08:56 -0500

    styling & tooltips for download buttons

---
+++ src/lib/icons/DownloadIcon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let size: number | string | undefined = undefined
+  let class_: string | undefined = undefined
+  export { class_ as class }
+</script>
+
+<svg
+  stroke="currentColor"
+  fill="currentColor"
+  stroke-width="0"
+  viewBox="0 0 20 20"
+  aria-hidden="true"
+  height={size ?? '100%'}
+  width={size ?? '100%'}
+  xmlns="http://www.w3.org/2000/svg"
+  class={class_}
+>
+  <path
+    fill-rule="evenodd"
+    d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+    clip-rule="evenodd"
+  />
+</svg>
--- src/routes/search/SearchResult.svelte
+++ src/routes/search/SearchResult.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { tooltipAction, TooltipDefaults } from '$lib/action-tooltip'
+  import DownloadIcon from '$lib/icons/DownloadIcon.svelte'
   import { getContextClient, type RouterOutput } from '$lib/trpc'
+  import { followCursor } from 'tippy.js'
 
   type SearchResult = RouterOutput['search'][number]
 
@@ -14,7 +17,16 @@
 </script>
 
 <div class="w-[200px]">
-  <button class="relative h-[200px] w-full shadow" on:click={handleDownload}>
+  <button
+    class="relative h-[200px] w-full shadow"
+    on:click={handleDownload}
+    use:tooltipAction={{
+      content: 'Download',
+      delay: [TooltipDefaults.delay, 0],
+      followCursor: true,
+      plugins: [followCursor],
+    }}
+  >
     {#if result.artwork?.[200]}
       <img
         class="h-full w-full rounded object-cover"
@@ -25,8 +37,10 @@
       <div class="center h-full w-full rounded bg-gray-800 italic text-gray-600">No cover art</div>
     {/if}
     <div
-      class="absolute top-0 left-0 h-full w-full rounded border border-white border-opacity-20"
-    />
+      class="center group absolute top-0 left-0 h-full w-full rounded border border-white border-opacity-20 transition hover:border-primary-500 hover:border-opacity-100 hover:bg-gray-900 hover:bg-opacity-60"
+    >
+      <DownloadIcon size={32} class="opacity-0 transition group-hover:opacity-100" />
+    </div>
   </button>
 
   <div class="truncate text-sm font-bold" title={result.title}>{result.title}</div>
