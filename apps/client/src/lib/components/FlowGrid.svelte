93af851f44084f19861fc3c1d7addf73097ed44f
Jake Cheek <jake.cheek@itential.com>
2023-03-18 01:16:44 -0400

    use built-in auto grid sizing

--- apps/client/src/lib/components/FlowGrid.svelte
+++ apps/client/src/lib/components/FlowGrid.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
-  export let targetColumnWidth = 200;
-
-  let clientWidth: number | undefined;
-  let numColumns = 1;
-  $: {
-    if (clientWidth !== undefined) {
-      numColumns = getColumnCount(clientWidth, targetColumnWidth);
-    } else {
-      numColumns = 1;
-    }
-  }
-
-  function getColumnCount(screenWidth: number, targetColumnWidth: number) {
-    const columnCount = Math.floor(screenWidth / targetColumnWidth);
-    const columnWidth = screenWidth / columnCount;
-    const delta1 = Math.abs(columnWidth - targetColumnWidth);
-    const delta2 = Math.abs(screenWidth / (columnCount + 1) - targetColumnWidth);
-    return delta1 < delta2 ? columnCount : columnCount + 1;
-  }
+  export let minColumnWidth = 150;
 </script>
 
 <div
-  bind:clientWidth
   class="grid gap-4 p-4"
-  style="grid-template-columns: repeat({numColumns}, 1fr);"
+  style="grid-template-columns: repeat(auto-fit, minmax({minColumnWidth}px, 1fr));"
 >
   <slot />
 </div>
