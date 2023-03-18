130bf5705153efe775455f7653e286aecb298017
Jake Cheek <jake.cheek@itential.com>
2023-03-17 22:45:09 -0400

    server watch: ignore node_modules and .turbo dirs

--- apps/server/tsup.config.dev.ts
+++ apps/server/tsup.config.dev.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'tsup';
+
 import baseConfig from './tsup.config.base';
 
 export default defineConfig({
   ...baseConfig,
-  watch: [
-    'src',
-    '../../packages/trpc/**/*',
-    '../../packages/db/**/*',
-    '../../packages/music-metadata/**/*'
-  ],
+  watch: ['src', '../../packages/trpc', '../../packages/db', '../../packages/music-metadata'],
+  ignoreWatch: ['../../**/node_modules/**/*', '../../**/.turbo/**/*'],
   onSuccess: 'node dist/server.js'
 });
