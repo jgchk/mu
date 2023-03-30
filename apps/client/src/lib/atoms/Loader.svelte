08bb17dd02c5cfdc7855657c9e5d9d4d79dd0b2e
Jake Cheek <jake@f-m.fm>
2023-03-29 23:29:26 -0400

    improve loader styling

--- apps/client/src/lib/atoms/Loader.svelte
+++ apps/client/src/lib/atoms/Loader.svelte
@@ -5,10 +5,39 @@
   export { class_ as class }
 </script>
 
-<div
-  class={tw(
-    'h-full w-full animate-spin rounded-full border-2 border-transparent border-t-current',
-    class_
-  )}
-  {...$$restProps}
-/>
+<svg class={tw('spinner h-full w-full', class_)} viewBox="0 0 50 50">
+  <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="7" />
+</svg>
+
+<style>
+  .spinner {
+    animation: rotate 2s linear infinite;
+  }
+
+  .spinner .path {
+    stroke: currentColor;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 150;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -35;
+    }
+    100% {
+      stroke-dasharray: 90, 150;
+      stroke-dashoffset: -124;
+    }
+  }
+</style>
