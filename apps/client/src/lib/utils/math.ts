d4988fbe8edaa8ea66bd386254c478a9de8c0ac6
Jake Cheek <jake@f-m.fm>
2023-03-29 23:30:33 -0400

    show loading on play button when track is loading

--- apps/client/src/lib/utils/math.ts
+++ apps/client/src/lib/utils/math.ts
@@ -1,3 +1,11 @@
 export const sum = (arr: number[]): number => {
   return arr.reduce((acc, cur) => acc + cur, 0)
 }
+
+export const isInRangeExclusive = (value: number, min: number, max: number): boolean => {
+  return value > min && value < max
+}
+
+export const isInOneOfRangesExclusive = (value: number, ranges: [number, number][]): boolean => {
+  return ranges.some(([min, max]) => isInRangeExclusive(value, min, max))
+}
