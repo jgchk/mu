44dfc298eb4658ddbd5a3c36d6fe221ab8ab1c7f
Jake Cheek <jake@f-m.fm>
2023-04-06 00:37:43 -0400

    animate icon

--- apps/client/src/lib/icons/PlayingIcon.svelte
+++ apps/client/src/lib/icons/PlayingIcon.svelte
@@ -12,7 +12,17 @@
   width={size ?? '100%'}
   class={class_}
 >
-  <rect x="2" y="6" rx="1.5" ry="1.5" width="3" height="11" />
-  <rect x="8" y="3" rx="1.5" ry="1.5" width="3" height="14" />
-  <rect x="14" y="10" rx="1.5" ry="1.5" width="3" height="7" />
+  <g transform="translate(0,20)">
+    <g transform="scale(1,-1)">
+      <rect x="2" y="3" rx="1.5" ry="1.5" width="3" height="11">
+        <animate attributeName="height" values="10;12;10" dur="1.5s" repeatCount="indefinite" />
+      </rect>
+      <rect x="8" y="3" rx="1.5" ry="1.5" width="3" height="14">
+        <animate attributeName="height" values="13;15;13" dur="2s" repeatCount="indefinite" />
+      </rect>
+      <rect x="14" y="3" rx="1.5" ry="1.5" width="3" height="7">
+        <animate attributeName="height" values="6;8;6" dur="1s" repeatCount="indefinite" />
+      </rect>
+    </g>
+  </g>
 </svg>
