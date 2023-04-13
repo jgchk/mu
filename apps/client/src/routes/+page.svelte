cc6905ec14fd0cec0318406de18f1947bf8933c6
Jake Cheek <jake@f-m.fm>
2023-04-13 00:03:59 -0400

    remove old restart soulseek button now that we have a dedicated status page

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-  import Button from '$lib/atoms/Button.svelte'
-  import { createRestartSoulseekMutation } from '$lib/services/system'
-  import { getContextClient } from '$lib/trpc'
-
-  const trpc = getContextClient()
-  const restartSoulseekMutation = createRestartSoulseekMutation(trpc)
-</script>
-
 <div>hi :)</div>
-<Button on:click={() => $restartSoulseekMutation.mutate()}>Restart Soulseek</Button>
