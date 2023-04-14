72b554b6ea2f97bd29cc3475138249dfe5bc58b5
Jake Cheek <jake@f-m.fm>
2023-04-14 17:48:01 -0400

    move library schema to separate file

---
+++ packages/db/src/schema/library.ts
@@ -0,0 +1,71 @@
+import type { InferModel } from 'drizzle-orm'
+import { integer, primaryKey, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
+
+export const artists = sqliteTable('artists', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+})
+export type Artist = InferModel<typeof artists>
+export type InsertArtist = InferModel<typeof artists, 'insert'>
+
+export const releases = sqliteTable('releases', {
+  id: integer('id').primaryKey(),
+  title: text('title'),
+})
+export type Release = InferModel<typeof releases>
+export type InsertRelease = InferModel<typeof releases, 'insert'>
+
+export const releaseArtists = sqliteTable(
+  'release_artists',
+  {
+    releaseId: integer('release_id')
+      .references(() => releases.id)
+      .notNull(),
+    artistId: integer('artist_id')
+      .references(() => artists.id)
+      .notNull(),
+    order: integer('order').notNull(),
+  },
+  (releaseArtists) => ({
+    releaseArtistsPrimaryKey: primaryKey(releaseArtists.releaseId, releaseArtists.artistId),
+  })
+)
+export type ReleaseArtist = InferModel<typeof releaseArtists>
+export type InsertReleaseArtist = InferModel<typeof releaseArtists, 'insert'>
+
+export const tracks = sqliteTable(
+  'tracks',
+  {
+    id: integer('id').primaryKey(),
+    path: text('path').notNull(),
+    title: text('title'),
+    releaseId: integer('release_id').references(() => releases.id),
+    trackNumber: integer('track_number'),
+    coverArtHash: text('cover_art_hash'),
+    duration: integer('duration').notNull(),
+    favorite: integer('favorite').notNull(),
+  },
+  (tracks) => ({
+    pathUniqueIndex: uniqueIndex('pathUniqueIndex').on(tracks.path),
+  })
+)
+export type Track = InferModel<typeof tracks>
+export type InsertTrack = InferModel<typeof tracks, 'insert'>
+
+export const trackArtists = sqliteTable(
+  'track_artists',
+  {
+    trackId: integer('track_id')
+      .references(() => tracks.id)
+      .notNull(),
+    artistId: integer('artist_id')
+      .references(() => artists.id)
+      .notNull(),
+    order: integer('order').notNull(),
+  },
+  (trackArtists) => ({
+    trackArtistsPrimaryKey: primaryKey(trackArtists.trackId, trackArtists.artistId),
+  })
+)
+export type TrackArtist = InferModel<typeof trackArtists>
+export type InsertTrackArtist = InferModel<typeof trackArtists, 'insert'>
