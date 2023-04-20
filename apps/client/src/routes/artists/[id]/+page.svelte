a0658daba8ee9422d3d00ea75d8709390b0dc5f5
Jake Cheek <jake@f-m.fm>
2023-04-19 23:43:16 -0400

    show artist header

--- apps/client/src/routes/artists/[id]/+page.svelte
+++ apps/client/src/routes/artists/[id]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { isNotNull } from 'utils'
+
   import CoverArt from '$lib/components/CoverArt.svelte'
   import FlowGrid from '$lib/components/FlowGrid.svelte'
-  import { makeImageUrl } from '$lib/cover-art'
+  import { makeCollageUrl, makeImageUrl } from '$lib/cover-art'
   import { createFullArtistQuery } from '$lib/services/artists'
   import { getContextClient } from '$lib/trpc'
 
@@ -14,10 +16,37 @@
 </script>
 
 {#if $artistQuery.data}
-  <div class="p-4 pt-0">
-    <h2 class="mb-4 mt-4 text-2xl font-bold">Releases</h2>
+  {@const artist = $artistQuery.data}
+  {@const imageIds = artist.releases
+    .map((r) => r.imageId)
+    .concat(artist.tracks.map((t) => t.imageId))
+    .filter(isNotNull)}
+
+  <div class="p-4">
+    <div class="relative flex items-end gap-6">
+      <div class="relative w-64 shrink-0">
+        <CoverArt
+          src={artist.imageId !== null
+            ? makeImageUrl(artist.imageId, { size: 512 })
+            : makeCollageUrl(imageIds, { size: 512 })}
+          alt={artist.name}
+          hoverable={false}
+        />
+      </div>
+
+      <div class="space-y-1 pb-2">
+        <h1
+          class="mr-11 line-clamp-2 break-all text-6xl font-bold leading-[1.19]"
+          title={artist.name}
+        >
+          {artist.name}
+        </h1>
+      </div>
+    </div>
+
+    <h2 class="mb-4 mt-8 text-2xl font-bold">Releases</h2>
     <FlowGrid>
-      {#each $artistQuery.data.releases as release (release.id)}
+      {#each artist.releases as release (release.id)}
         <div class="w-full">
           <a href="/releases/{release.id}" class="w-full">
             <CoverArt
@@ -37,9 +66,9 @@
       {/each}
     </FlowGrid>
 
-    <h2 class="mb-4 mt-16 text-2xl font-bold">Tracks</h2>
+    <h2 class="mb-4 mt-12 text-2xl font-bold">Tracks</h2>
     <FlowGrid>
-      {#each $artistQuery.data.tracks as track (track.id)}
+      {#each artist.tracks as track (track.id)}
         <div class="w-full">
           <a href="/releases/{track.releaseId}" class="w-full">
             <CoverArt
