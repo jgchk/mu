8230cbd0669747fc261e2b011f2ab21c7b59a7ab
Jake Cheek <jake@f-m.fm>
2023-04-22 13:30:25 -0400

    add loading state to queue

--- apps/client/src/lib/components/Queue.svelte
+++ apps/client/src/lib/components/Queue.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition'
   import { equalsWithOrder, isDefined } from 'utils'
 
   import { autoscroll } from '$lib/actions/autoscroll'
+  import Loader from '$lib/atoms/Loader.svelte'
   import { nowPlaying } from '$lib/now-playing'
   import { createFavoriteTrackMutation, createTracksQuery } from '$lib/services/tracks'
   import { getContextClient } from '$lib/trpc'
 
+  import Delay from './Delay.svelte'
   import TrackList from './TrackList.svelte'
 
   const trpc = getContextClient()
@@ -33,6 +36,9 @@
     {@const previousTracks = $tracksQuery.data.filter((t) =>
       $nowPlaying.previousTracks.includes(t.id)
     )}
+    {@const nowPlayingTrack = $tracksQuery.data.find((t) => t.id === $nowPlaying.track?.id)}
+    {@const nextTracks = $tracksQuery.data.filter((t) => $nowPlaying.nextTracks.includes(t.id))}
+
     {#if previousTracks.length}
       <div>
         <h6 class="mb-1 font-bold text-gray-400">Previous</h6>
@@ -43,12 +49,9 @@
         />
       </div>
     {/if}
-  {/if}
 
-  <div use:autoscroll>
-    <h6 class="text-primary-600 mb-1 font-bold">Now playing</h6>
-    {#if $tracksQuery.data}
-      {@const nowPlayingTrack = $tracksQuery.data.find((t) => t.id === $nowPlaying.track?.id)}
+    <div use:autoscroll>
+      <h6 class="text-primary-600 mb-1 font-bold">Now playing</h6>
       {#if nowPlayingTrack}
         <TrackList
           tracks={[nowPlayingTrack]}
@@ -56,11 +59,8 @@
             $favoriteMutation.mutate({ id: e.detail.track.id, favorite: e.detail.favorite })}
         />
       {/if}
-    {/if}
-  </div>
+    </div>
 
-  {#if $tracksQuery.data}
-    {@const nextTracks = $tracksQuery.data.filter((t) => $nowPlaying.nextTracks.includes(t.id))}
     {#if nextTracks.length}
       <div>
         <h6 class="mb-1 font-bold text-gray-400">Next</h6>
@@ -71,5 +71,13 @@
         />
       </div>
     {/if}
+  {:else if $tracksQuery.error}
+    <div class="text-red-500">Error: {$tracksQuery.error.message}</div>
+  {:else}
+    <Delay>
+      <div class="flex h-full max-h-72 items-center justify-center" in:fade|local>
+        <Loader class="h-10 w-10 text-gray-600" />
+      </div>
+    </Delay>
   {/if}
 </div>
