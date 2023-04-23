567ec8474e361c200d540f7453d7eacd3038c403
Jake Cheek <jake@f-m.fm>
2023-04-23 16:42:18 -0400

    add padding to tag page

--- apps/client/src/routes/tags/[id]/+page@.svelte
+++ apps/client/src/routes/tags/[id]/+page@.svelte
@@ -58,66 +58,71 @@
     {/if}
   </svelte:fragment>
 
-  {#if $tagQuery.data}
-    {@const tag = $tagQuery.data}
-    <div class="relative flex items-end gap-6">
-      <div class="space-y-1 pb-2">
-        <h1 class="mr-11 line-clamp-2 break-all text-6xl font-bold leading-[1.19]" title={tag.name}>
-          {tag.name}
-        </h1>
-        {#if tag.description}
-          <p
-            class="line-clamp-5 whitespace-pre-wrap leading-[1.19] text-gray-400"
-            title={tag.description}
+  <div class="p-4">
+    {#if $tagQuery.data}
+      {@const tag = $tagQuery.data}
+      <div class="relative flex items-end gap-6">
+        <div class="space-y-1 pb-2">
+          <h1
+            class="mr-11 line-clamp-2 break-all text-6xl font-bold leading-[1.19]"
+            title={tag.name}
           >
-            {tag.description}
-          </p>
-        {/if}
-      </div>
-    </div>
-  {/if}
-
-  <h2 class="mb-4 mt-8 text-2xl font-bold">Releases</h2>
-  {#if $releasesQuery.data}
-    {@const releases = $releasesQuery.data}
-    <FlowGrid>
-      {#each releases as release (release.id)}
-        <div class="w-full">
-          <a href="/releases/{release.id}" class="w-full">
-            <CoverArt
-              src={release.imageId !== null
-                ? makeImageUrl(release.imageId, { size: 512 })
-                : undefined}
-            />
-          </a>
-          <a
-            href="/releases/{release.id}"
-            class="mt-1 block truncate font-medium hover:underline"
-            title={release.title}
-          >
-            {release.title}
-          </a>
+            {tag.name}
+          </h1>
+          {#if tag.description}
+            <p
+              class="line-clamp-5 whitespace-pre-wrap leading-[1.19] text-gray-400"
+              title={tag.description}
+            >
+              {tag.description}
+            </p>
+          {/if}
         </div>
-      {/each}
-    </FlowGrid>
-  {:else if $releasesQuery.error}
-    <div>{$releasesQuery.error.message}</div>
-  {:else}
-    <div>Loading...</div>
-  {/if}
+      </div>
+    {/if}
 
-  <h2 class="mb-4 mt-12 text-2xl font-bold">Tracks</h2>
-  {#if $tracksQuery.data}
-    {@const tracks = $tracksQuery.data}
-    <TrackList
-      {tracks}
-      on:play={(e) => playTrack(e.detail.track.id, makeQueueData(tracks, e.detail.i))}
-      on:favorite={(e) =>
-        $favoriteMutation.mutate({ id: e.detail.track.id, favorite: e.detail.favorite })}
-    />
-  {:else if $tracksQuery.error}
-    <div>{$tracksQuery.error.message}</div>
-  {:else}
-    <div>Loading...</div>
-  {/if}
+    <h2 class="mb-4 mt-8 text-2xl font-bold">Releases</h2>
+    {#if $releasesQuery.data}
+      {@const releases = $releasesQuery.data}
+      <FlowGrid>
+        {#each releases as release (release.id)}
+          <div class="w-full">
+            <a href="/releases/{release.id}" class="w-full">
+              <CoverArt
+                src={release.imageId !== null
+                  ? makeImageUrl(release.imageId, { size: 512 })
+                  : undefined}
+              />
+            </a>
+            <a
+              href="/releases/{release.id}"
+              class="mt-1 block truncate font-medium hover:underline"
+              title={release.title}
+            >
+              {release.title}
+            </a>
+          </div>
+        {/each}
+      </FlowGrid>
+    {:else if $releasesQuery.error}
+      <div>{$releasesQuery.error.message}</div>
+    {:else}
+      <div>Loading...</div>
+    {/if}
+
+    <h2 class="mb-4 mt-12 text-2xl font-bold">Tracks</h2>
+    {#if $tracksQuery.data}
+      {@const tracks = $tracksQuery.data}
+      <TrackList
+        {tracks}
+        on:play={(e) => playTrack(e.detail.track.id, makeQueueData(tracks, e.detail.i))}
+        on:favorite={(e) =>
+          $favoriteMutation.mutate({ id: e.detail.track.id, favorite: e.detail.favorite })}
+      />
+    {:else if $tracksQuery.error}
+      <div>{$tracksQuery.error.message}</div>
+    {:else}
+      <div>Loading...</div>
+    {/if}
+  </div>
 </Layout>
