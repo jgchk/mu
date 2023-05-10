93256168c9dde9200fec63e9d8917cae9d4f02bf
Jake Cheek <jake@f-m.fm>
2023-05-09 21:40:46 -0400

    fix build error

--- apps/client/src/lib/components/TrackListTrack.svelte
+++ apps/client/src/lib/components/TrackListTrack.svelte
@@ -88,6 +88,6 @@
     {/if}
     <FavoriteButton layer={700} favorite={track.favorite} on:click={() => favorite()} />
     <AddToPlaylistButton trackId={track.id} layer={700} />
-    <TrackListTrackTagsButton trackId={track.id} layer={700} hasTags={track.hasTags} />
+    <TrackListTrackTagsButton trackId={track.id} layer={700} />
   </div>
 </div>
