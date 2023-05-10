2bc90280155938437ba07d73290199c9fd10f46d
Jake Cheek <jake@f-m.fm>
2023-05-10 16:07:04 -0400

    fix sidebar inserts on small screen sizes

--- apps/client/src/routes/(library)/+layout.svelte
+++ apps/client/src/routes/(library)/+layout.svelte
@@ -9,19 +9,21 @@
 
 <div class="flex h-full gap-2 flex-col md:flex-row">
   <div
-    class="overflow-auto rounded bg-gray-900 py-2 order-2 md:order-1 flex flex-row md:flex-col w-full md:w-48 justify-around md:justify-start"
+    class="overflow-auto rounded bg-gray-900 py-2 order-2 md:order-1 flex md:flex-col w-full md:w-48 justify-around md:justify-start"
   >
     <LibraryLink href="/playlists" label="Playlists"><CollectionIcon /></LibraryLink>
     <LibraryLink href="/artists" label="Artists"><PersonIcon /></LibraryLink>
     <LibraryLink href="/releases" label="Releases"><AlbumIcon /></LibraryLink>
     <LibraryLink href="/tracks" label="Tracks"><MusicNoteIcon /></LibraryLink>
 
-    {#if $$slots.sidebar}
-      <div class="p-2">
-        <div class="h-px bg-gray-800" />
-      </div>
-      <slot name="sidebar" />
-    {/if}
+    <div class="flex md:flex-col">
+      {#if $$slots.sidebar}
+        <div class="pr-2 md:pr-2 md:p-2">
+          <div class="w-px h-full md:w-full md:h-px bg-gray-800" />
+        </div>
+        <slot name="sidebar" />
+      {/if}
+    </div>
   </div>
 
   <div class="h-full flex-1 overflow-auto order-1 md:order-2">
