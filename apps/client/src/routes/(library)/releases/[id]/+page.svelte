6d170a88adcf1bd3f91983b8feeae60da98e9473
Jake Cheek <jake@f-m.fm>
2023-05-11 19:31:06 -0400

    extract library header to common component

--- apps/client/src/routes/(library)/releases/[id]/+page.svelte
+++ apps/client/src/routes/(library)/releases/[id]/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
   import CommaList from '$lib/atoms/CommaList.svelte'
   import LinkButton from '$lib/atoms/LinkButton.svelte'
-  import CoverArt from '$lib/components/CoverArt.svelte'
   import FullscreenLoader from '$lib/components/FullscreenLoader.svelte'
   import TrackList from '$lib/components/TrackList.svelte'
   import { makeImageUrl } from '$lib/cover-art'
-  import PlayIcon from '$lib/icons/PlayIcon.svelte'
   import { playTrack } from '$lib/now-playing'
   import { createReleaseTracksQuery, createReleaseWithArtistsQuery } from '$lib/services/releases'
   import { createFavoriteTrackMutation } from '$lib/services/tracks'
   import type { RouterOutput } from '$lib/trpc'
   import { getContextClient } from '$lib/trpc'
 
+  import Header from '../../Header.svelte'
   import type { PageData } from './$types'
   import Tags from './Tags.svelte'
 
@@ -36,47 +35,24 @@
   {@const release = $releaseQuery.data}
 
   <div class="space-y-4">
-    <div class="group/tags relative flex flex-col items-center gap-6 sm:flex-row sm:items-end">
-      <button
-        type="button"
-        class="relative w-64 max-w-full shrink-0 overflow-hidden sm:w-32 lg:w-48 xl:w-64"
-        disabled={!tracks?.length}
-        on:click={() => tracks && playTrack(tracks[0].id, makeQueueData(tracks, 0))}
-      >
-        <CoverArt
-          src={release.imageId !== null ? makeImageUrl(release.imageId, { size: 512 }) : undefined}
-          alt={release.title}
-          iconClass="w-8 h-8 lg:w-16 lg:h-16"
-          hoverable={!!tracks?.length}
-        >
-          <PlayIcon />
-        </CoverArt>
-      </button>
-
-      <div class="space-y-1 self-start px-1.5 pb-2 md:px-0">
-        <h1
-          class="mr-11 line-clamp-2 break-all text-2xl font-bold leading-[1.19] sm:text-3xl md:text-4xl xl:text-5xl 2xl:text-6xl"
-          title={release.title}
-        >
-          {release.title}
-        </h1>
-        <div class="flex items-center gap-2 text-sm">
-          <CommaList class="text-sm font-bold" items={release.artists} let:item>
-            <a class="hover:underline" href="/artists/{item.id}">{item.name}</a>
-          </CommaList>
-          •
-          <Tags releaseId={data.id} />
-        </div>
+    <Header
+      title={release.title}
+      coverArtSrc={release.imageId !== null
+        ? makeImageUrl(release.imageId, { size: 512 })
+        : undefined}
+      coverArtClickable={!!tracks?.length}
+      class="group/tags"
+    >
+      <div class="flex items-center gap-2 text-sm" slot="subtitle">
+        <CommaList class="text-sm font-bold" items={release.artists} let:item>
+          <a class="hover:underline" href="/artists/{item.id}">{item.name}</a>
+        </CommaList>
+        •
+        <Tags releaseId={data.id} />
       </div>
 
-      <LinkButton
-        href="/releases/{release.id}/edit"
-        kind="outline"
-        class="absolute bottom-4 right-0 md:bottom-[unset] md:top-0"
-      >
-        Edit
-      </LinkButton>
-    </div>
+      <LinkButton href="/releases/{release.id}/edit" kind="outline" slot="buttons">Edit</LinkButton>
+    </Header>
 
     {#if $tracksQuery.data}
       {@const tracks = $tracksQuery.data}
