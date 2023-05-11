eb6d5b2407e525efd389e003f77405b60f83ff51
Jake Cheek <jake@f-m.fm>
2023-05-11 12:04:29 -0400

    run dev server on local network

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -1,18 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from '@sveltejs/kit/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
   server: {
+    host: process.env.SERVER_HOST,
     port: process.env.DEV_PORT,
     proxy: {
       '/api': {
         target: `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`,
         changeOrigin: true,
-        ws: true
+        ws: true,
       },
-    }
+    },
   },
-};
+}
 
-export default config;
+export default config
