3049952db3e93cfbd2683d0ac5f50bbe63f96b76
Jake Cheek <jake@f-m.fm>
2023-10-11 19:39:43 -0400

    db: remove unused listening props

--- packages/db/src/schema/accounts.ts
+++ packages/db/src/schema/accounts.ts
@@ -1,8 +1,6 @@
 import type { InferModel } from 'drizzle-orm'
 import { integer, sqliteTable, text, uniqueIndex } from 'drizzle-orm/sqlite-core'
 
-import { tracks } from './tracks'
-
 export type Account = InferModel<typeof accounts>
 export type InsertAccount = InferModel<typeof accounts, 'insert'>
 export const accounts = sqliteTable(
@@ -12,13 +10,6 @@ export const accounts = sqliteTable(
     username: text('username').notNull(),
     passwordHash: text('password_hash').notNull(),
     createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-
-    listeningTrackId: integer('listening_track_id').references(() => tracks.id),
-    listeningTrackStartedAt: integer('listening_track_started_at', {
-      mode: 'timestamp',
-    }),
-    listeningTrackCurrentTime: integer('listening_track_current_time'),
-    listeningTrackScrobbled: integer('listening_track_scrobbled', { mode: 'boolean' }),
   },
   (accounts) => ({
     accountsUsernameUniqueIndex: uniqueIndex('accountsUsernameUniqueIndex').on(accounts.username),
