2c60a7a48f303cc22d402d139d7f81c87cfb5703
Jake Cheek <jake@f-m.fm>
2023-10-11 21:30:06 -0400

    client: don't require scrolling to get to new tag button

--- apps/client/src/routes/(logged-in)/tags/+layout.svelte
+++ apps/client/src/routes/(logged-in)/tags/+layout.svelte
@@ -6,11 +6,15 @@
 </script>
 
 <div class="flex h-full gap-2">
-  <div class="flex w-48 flex-col overflow-auto rounded bg-gray-900 p-2">
-    <div class="flex-1 px-1">
+  <div class="flex w-48 flex-col rounded bg-gray-900 p-2">
+    <div class="flex-1 overflow-auto px-1">
       <slot name="sidebar" />
     </div>
-    <Button kind="outline" on:click={() => dialogs.open('new-tag')} class="mt-1">New Tag</Button>
+    <div class="border-t border-gray-800 pt-1">
+      <Button kind="outline" on:click={() => dialogs.open('new-tag')} class="mt-1 w-full">
+        New Tag
+      </Button>
+    </div>
   </div>
 
   <div class="h-full flex-1 overflow-auto">
