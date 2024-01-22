4ffe178ef3369ba6176eeb318bde6013ef5d4993
Jake Cheek <jake@f-m.fm>
2024-01-22 17:55:24 -0500

    android: open app upon clicking notification

--- apps/android/app/src/main/java/cafe/jake/mu/NotificationManager.kt
+++ apps/android/app/src/main/java/cafe/jake/mu/NotificationManager.kt
@@ -3,7 +3,9 @@ package cafe.jake.mu
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.media3.common.util.UnstableApi
@@ -35,11 +37,16 @@ class NotificationManager(private val context: Context, private val player: ExoP
 
     @UnstableApi
     private fun buildNotification(mediaSession: MediaSession) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+
         PlayerNotificationManager.Builder(context, NOTIFICATION_ID, NOTIFICATION_CHANNEL_ID)
             .setMediaDescriptionAdapter(
                 NotificationAdapter(
                     context = context,
-                    pendingIntent = mediaSession.sessionActivity
+                    pendingIntent = pendingIntent
                 )
             )
             .build()
